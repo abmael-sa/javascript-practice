ead03b62b161bff949dc121151eb41747bfcad89
Abmael <dante.sawano@gmail.com>
2022-06-02 18:17:38 -0300

    give the tools the functionality to be removed

--- chaordic/src/domain/entities/tool/tool.js
+++ chaordic/src/domain/entities/tool/tool.js
@@ -1,20 +1,38 @@
+import { removeTool } from "../remove";
 
-export const tool = (core) => {
+
+export const tool = (data) => {
     const div = document.createElement('div');
     const button = document.createElement('button');
     const p = document.createElement('p');
     const p2 = document.createElement('p');
     const h3 = document.createElement('h3');
-
+    
     button.textContent = 'X';
-    p.textContent = core.description;
-    p2.textContent = core.tags;
-    h3.textContent = core.title;
+    p.textContent = data.description;
+    p2.textContent = data.tags;
+    h3.textContent = data.title;
 
     div.append(h3);
     div.append(p);
     div.append(p2);
     div.append(button);
+
+    div.setAttribute('id', data.id);
+
+    button.addEventListener('click', (event) => {
+        const button = event.target;
+        const parent = button.parentElement;
+        const commonAncestor = parent.parentElement;
+
+        const id = parent.id;
+
+        console.log(id);
+
+        removeTool(id);
+
+        commonAncestor.removeChild(parent);
+    });
 
     return div;
 }
